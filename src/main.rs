191c0ab71d8b0f382be1fbb40a1241dd801c1f6a
YotioSoft <ytani0323@gmail.com>
2023-10-07 10:42:24 +0900

    temp

--- src/main.rs
+++ src/main.rs
@@ -35,6 +35,38 @@ fn get_system_procs_info(mut buffer_size: u32) -> *mut c_void {
     }
 }
 
+// SystemHandleInformation を buffer に取得
+fn get_system_handle_info(mut buffer_size: u32) -> *mut c_void {
+    unsafe {
+        let mut base_address = VirtualAlloc(std::ptr::null_mut(), buffer_size as usize, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+
+        let tries = 0;
+        let max_tries = 5;
+        loop {
+            // プロセス情報を取得
+            // SystemHandleInformation  : 各ハンドルの情報（オプション定数）
+            // base_address             : 格納先
+            // buffer_size              : 格納先のサイズ
+            // &mut buffer_size         : 実際に取得したサイズ
+            let res = NtQuerySystemInformation(SystemHandleInformation, base_address, buffer_size, &mut buffer_size);
+            
+            if res == 0 {
+                break;
+            }
+            if tries == max_tries {
+                break;
+            }
+
+            // realloc
+            VirtualFree(base_address, 0, MEM_RELEASE);
+            base_address = VirtualAlloc(std::ptr::null_mut(), buffer_size as usize, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+        }
+
+        base_address
+    }
+}
+
+
 fn get_proc_info(next_address: isize) -> SYSTEM_PROCESS_INFORMATION {
     unsafe {
         let mut system_process_info: SYSTEM_PROCESS_INFORMATION = std::mem::zeroed();
@@ -69,7 +101,7 @@ fn get_proc_id(proc_info: SYSTEM_PROCESS_INFORMATION) -> u32 {
 fn main() {
     unsafe {
         // プロセス情報を取得
-        let base_address = get_system_procs_info(0x10000);
+        let base_address = get_system_handle_info(0x10000);
 
         // base_address に取得したプロセス情報を SYSTEM_PROCESS_INFORMATION 構造体 system_process_info に格納
         let mut system_process_info = get_proc_info(base_address as isize);
@@ -81,16 +113,10 @@ fn main() {
             next_address += system_process_info.NextEntryOffset as isize;
 
             // base_address に取得したプロセス情報を SYSTEM_PROCESS_INFORMATION 構造体 system_process_info に格納
-            system_process_info = get_proc_info(next_address);
-            
-            // プロセス名を取得
-            let proc_name = get_proc_name(system_process_info);
-
-            // プロセスIDを取得
-            let proc_id = get_proc_id(system_process_info);
+            let system_handle_info = next_address as SYSTEM_PROCESS_INFORMATION;
             
             // プロセス名とプロセスIDを表示
-            println!("pid {} - {}", proc_id, proc_name);
+            println!("hwnd {:?}", system_handle_info.UniqueProcessId);
 
             // すべてのプロセス情報を取得したら終了
             if system_process_info.NextEntryOffset == 0 {
